0a594ed66ddc36c74816b96d1d416774a0eb361a
amaurysr <arodriguez5534@gmail.com>
2023-11-12 19:27:44 -0500

    added progress bar

--- main.go
+++ main.go
@@ -5,13 +5,16 @@ import (
 	"go-project-manager/pkg/mainreader"
 	"go-project-manager/pkg/jsonconvert"
 	"go-project-manager/pkg/progresschanger"
+	"github.com/schollz/progressbar/v3"
 )
 	
 func main(){
 	var list = mainreader.Listpaste()
-	fmt.Println("Current Project: ",list.Datalist[0].ProjectName)
-	fmt.Println("Your Progress: ",list.Datalist[0].Progress)
-	fmt.Println("Project Members: ",list.Datalist[0].ProjectMembers)
+	fmt.Println("\n\nCurrent Project: ",list.Datalist[0].ProjectName)
+	fmt.Println("Your Progress: ")
+	bar := progressbar.NewOptions(100, progressbar.OptionSetWidth(list.Datalist[0].Progress), progressbar.OptionSetPredictTime(false))
+	bar.Add(list.Datalist[0].Progress)
+	fmt.Println("\nProject Members: ",list.Datalist[0].ProjectMembers)
 	var numb int;
 	numb = progresschanger.RequestChange(list.Datalist[0].Progress)
 	list.Datalist[0].Progress = numb
--- pkg/progresschanger/progresschanger.go
+++ pkg/progresschanger/progresschanger.go
@@ -8,14 +8,13 @@ import(
 func progressChanger(x int) (int){
    for {
         var entry int;
-        fmt.Println("Enter a number greater than the current percentage: \n")
+        fmt.Println("\n\nEnter a number greater than the current percentage: \n")
         fmt.Scanf("%d", &entry)
         if entry > x {
                 x = entry;
         	break;  
-        } else {
-        	fmt.Println("Enter a valid entry.")
-   	}
+        } 
+        fmt.Println("\n\nEnter a valid entry.")
    }
    return x;	
 }
@@ -23,16 +22,18 @@ func progressChanger(x int) (int){
 
 func RequestChange(x int) (int){
         for {
-        fmt.Println("Do you want to change your progress?\n")
+        fmt.Println("\n\nDo you want to change your progress?\n")
         var response string;
         fmt.Scanf("%s", &response)
-        if response == "yes" {
+        if response == "yes"{
                 x = progressChanger(x)
                 break;
-        } else if response == "no"{
+        }else if response == "no"{
                 fmt.Println("Exitting")
                 os.Exit(x)
-        }
+        }else{
+		fmt.Println("Enter a valid entry.")
+	}
         }
         return x;
 }
